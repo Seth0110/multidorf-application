cfea2f462dd350772dabfe3b304e2d6232cb72f8
Tim Miller <t.r.miller0823@gmail.com>
2018-05-11 10:49:01 -0400

    Added hardcoded dwarf fort version data

--- multidorf/bay12.py
+++ multidorf/bay12.py
@@ -1,8 +1,22 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import urllib.request
 
 
 def list_df():
-    f = urllib.request.urlopen('http://bay12games.com/dwarves/older_versions.html')
-    soup = BeautifulSoup(f.read(), 'html.parser')
-    return soup.body.find('p', attrs={'class':'menu'}).text
+    # f = urllib.request.urlopen('http://bay12games.com/dwarves/older_versions.html')
+    # soup = BeautifulSoup(f.read(), 'html.parser')
+    # return soup.body.find('p', attrs={'class':'menu'}).text
+    """For now we are hardcoding this in, and we only have a single version.  This is not the final behavior"""
+    return [dict(
+        version='0.44.10',
+        date='5/5/2018',
+        x86=dict(
+            windows='http://bay12games.com/dwarves/df_44_10_legacy32.zip',
+            windows_legacy='http://bay12games.com/dwarves/df_44_10_win32.zip',
+            linux='http://bay12games.com/dwarves/df_44_10_linux32.tar.bz2',
+            mac='http://bay12games.com/dwarves/df_44_10_osx32.tar.bz2'),
+        x86_64=dict(
+            windows='http://bay12games.com/dwarves/df_44_10_win.zip',
+            windows_legacy='http://bay12games.com/dwarves/df_44_10_legacy.zip',
+            linux='http://bay12games.com/dwarves/df_44_10_linux.tar.bz2',
+            mac='http://bay12games.com/dwarves/df_44_10_osx.tar.bz2'))]
