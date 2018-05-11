bc6aa6fa6aed7954680518d4a4b0171367b319c2
Tim Miller <t.r.miller0823@gmail.com>
2018-05-11 11:07:58 -0400

    Added more buttons for tkinter, currently all do nothing

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from tkinter import Tk, Label, Button, Frame, Listbox, LEFT, RIGHT, TOP, SINGLE,
 from multidorf import bay12, fs
 
 
-class MultiDorf():
+class MultiDorf:
     def __init__(self, master):
         self.master = master
-        master.title('MultiDorf VERSION/PLATFORM')
+        self.master.title('MultiDorf VERSION/PLATFORM')
 
         # self.label = Label(master, text='this is multidorf')
         # self.label.pack()
 
-        self.btn_frame = Frame(master)
+        self.btn_frame = Frame(master, background='light grey')
         self.btn_frame.pack(side=TOP)
 
         self.instance_btn = Button(self.btn_frame, text='New Instance', command=self.new_instance)
@@ -34,14 +34,59 @@ class MultiDorf():
         self.instance_lb = Listbox(self.instance_frame, selectmode=SINGLE)
         self.instance_lb.pack()
 
+        self.instance_opt_frame = Frame(master)
+        self.instance_opt_frame.pack(side=TOP)
+
+        self.instance_launch_btn = Button(self.instance_opt_frame, text='Launch', command=self.launch_instance)
+        self.instance_launch_btn.pack()
+
+        self.instance_edit_btn = Button(self.instance_opt_frame, text='Edit', command=self.edit_instance)
+        self.instance_edit_btn.pack()
+
+        self.instance_saves_btn = Button(self.instance_opt_frame, text='View saves', command=self.view_saves)
+        self.instance_saves_btn.pack()
+
+        self.instance_folder_btn = Button(self.instance_opt_frame, text='Instance folder', command=self.instance_folder)
+        self.instance_folder_btn.pack()
+
+        self.instance_copy_btn = Button(self.instance_opt_frame, text='Copy instance', command=self.copy_instance)
+        self.instance_copy_btn.pack()
+
+        self.instance_delete_btn = Button(self.instance_opt_frame, text='Delete instance', command=self.delete_instance)
+        self.instance_delete_btn.pack()
+
+        self.instance_export_btn = Button(self.instance_opt_frame, text='Export instance', command=self.export_instance)
+        self.instance_export_btn.pack()
+
+    def export_instance(self):
+        pass
+
+    def delete_instance(self):
+        pass
+
+    def copy_instance(self):
+        pass
+
     def help(self):
-        print('Help!!!')
+        pass
+
+    def view_saves(self):
+        pass
+
+    def instance_folder(self):
+        pass
+
+    def edit_instance(self):
+        pass
 
     def new_instance(self):
-        print('creating new thing')
+        pass
 
     def settings(self):
-        print('settings pressed')
+        pass
+
+    def launch_instance(self):
+        pass
 
 
 def main():
