25c2ee3746bc19fe1045f8adb9e8cae1a10ebf46
Tim Miller <t.r.miller0823@gmail.com>
2018-05-13 13:44:48 -0400

    Added some Mac constants for files.py

--- multidorf/files.py
+++ multidorf/files.py
@@ -2,18 +2,16 @@ from pathlib import Path
 from sys import platform
 import os
 import subprocess
-import json
 
 if platform == 'win32':
     data_root = Path(os.getenv('APPDATA')) / 'multidorf'
 elif platform == 'darwin':
-    data_root = None # TODO
+    data_root = Path.home() / 'Library' / 'multidorf'
 else:
     data_root = Path.home() / '.config/multidorf'
 
 instance_dir = data_root / 'instances'
 appconfig_file = data_root / 'appconfig.json'
-
 
 
 def delete_recursively(directory:Path):
@@ -29,6 +27,6 @@ def browse(directory:Path):
     if platform == 'win32':
         subprocess.Popen(r'explorer "' + str(directory) + '"')
     elif platform == 'darwin':
-        raise NotImplementedError
+        os.system('open ' + str(directory))
     else:
         subprocess.Popen('xdg-open ' + str(directory))
